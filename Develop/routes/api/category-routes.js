bf7d5d876704aa52fa1a4711938b8e3d80ea11f0
Tim Vigneau <tjvig94@gmail.com>
2021-03-14 17:33:32 -0400

    Categories can have names be updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -47,7 +47,20 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  try {
+    const updatedCategory = Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(updatedCategory => (updatedCategory) ? res.status(200).json(updatedCategory) : res.status(404).json({ message: "No category to update."}))
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
