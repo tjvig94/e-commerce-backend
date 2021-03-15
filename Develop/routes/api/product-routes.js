ac3153bc658aa021b067bd98c553fd9bae56bd36
Tim Vigneau <tjvig94@gmail.com>
2021-03-14 21:28:20 -0400

    Can find tags and products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,14 +7,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      attributes: ['id', 'product_name'],
+      include: [
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        },
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        }]
+    }).then(productData => (productData) ? res.status(200).json(productData) : res.status(404).json({ message: "No products found." }));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      ]
+    }).then(productData => (productData) ? res.status(200).json(productData) : res.status(404).json({ message: "No products found with that id." }));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// FIX THISSSSSSSSS!!!!!!!!!! Y DONT WORK
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
@@ -25,7 +58,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +129,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const destroyedProduct = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(destroyedProduct => (destroyedProduct) ? res.status(200).json(destroyedProduct) : res.status(404).json({ message: "No product to delete with that id."}))
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(tag => res.status(200).json(tag))
+  .catch(err => {
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(tag => (tag) ? res.status(200).json(tag) : res.status(400).json({ message: "No tag found with that id." }))
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
